6dfa48cb3c39c87edd1d82679aa8ac4f68738a42
manggala56 <manggalaputra740@gmail.com>
2023-09-29 18:32:24 +0700

    data seader

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(1)->create([
-            'name' => 'Velonic',
-            'email' => 'test@test.com',
+            'name' => 'wian',
+            'email' => 'wian@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
